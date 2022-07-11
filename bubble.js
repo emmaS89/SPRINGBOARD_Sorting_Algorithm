8fe1bd2b655579867bb4827f4be43c4c5b8694b4
emmaS89 <ataksinem89@gmail.com>
2022-07-11 13:38:14 -0400

    created

---
+++ bubble.js
@@ -0,0 +1,33 @@
+// function bubbleSort(arr) {
+//   const swap = (arr, idx1, idx2) => {
+//     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+//   };
+
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         swap(arr, j, j + 1);
+//       }
+//     }
+//   }
+
+//   return arr;
+// }
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+module.exports = bubbleSort;
+
+module.exports = bubbleSort;
